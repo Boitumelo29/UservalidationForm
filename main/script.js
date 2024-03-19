edf86ff2fe30bc93bef6a37b22122c9e6731c46d
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-03-19 14:58:19 +0200

    update

--- main/script.js
+++ main/script.js
@@ -4,39 +4,42 @@ var password = document.forms['form']['password'];
 var email_error = document.getElementById('email_error');
 var password_error = document.getElementById('password_error');
 
-email.addEventListener('textInput', email_verify);
-password.addEventListener('textInput', password_verify);
+email.addEventListener('input', email_verify);
+password.addEventListener('input', password_verify);
 
 function validate(){
-    if(email.value.length<9){
-        email.style.border = "1px solid red";
+    var isValid = true;
+
+    if(email.value.length < 1 || !email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) && !email.value.match(/^\d{10}$/)){
         email_error.style.display = "block";
-        email.focus();
-        return false;
+        isValid = false;
+    } else {
+        email_error.style.display = "none";
     }
 
-    if(password.value.length<6){
-        password.style.border = "1px solid red";
+    if(password.value.length < 6){
         password_error.style.display = "block";
-        password.focus();
-        return false;
+        isValid = false;
+    } else {
+        password_error.style.display = "none";
     }
+
+    return isValid;
 }
 
 function email_verify(){
-
-    if(email.value.length >= 8 ){
-        email.style.border = "1px solid red";
+    if(email.value.length >= 1 && (email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) || email.value.match(/^\d{10}$/))){
         email_error.style.display = "none";
-        return true;
     }
 }
 
 function password_verify(){
-
-    if(password.value.length >= 6 ){
-        password.style.border = "1px solid red";
+    if(password.value.length >= 6){
         password_error.style.display = "none";
-        return true;
     }
+}
+
+function togglePasswordVisibility() {
+    var passwordField = document.querySelector('input[name="password"]');
+    passwordField.type = document.getElementById('showPassword').checked ? 'text' : 'password';
 }
